6b6bd652cd0036a7f66a84794d69762a2aa9145b
sukufi <kaanm.toprak@gmail.com>
2023-03-02 21:56:00 +0300

    v0.2

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # combine_heat_and_power_calculator
+
+-v.01
+main idea created
+
+v.02
+regular expression added
+
+
+
+futureworks
+-using keys to append excel
--- chp_verim_hesaplama.py
+++ chp_verim_hesaplama.py
@@ -1,3 +1,4 @@
+import re
 import PySimpleGUI as sg
 from openpyxl import Workbook, load_workbook
 
@@ -10,20 +11,20 @@ ws = wb.active
 
 layout = [
 
-    [sg.Text("Please fill out information below")],
+    [sg.Text("Please fill in the blanks with the monthly electricity consumption in kWh.")],
     
-    [sg.Text("Ocak Ayı:", size=(15,1)), sg.InputText(key = "Ocak")],
-    [sg.Text("Şubat Ayı:", size=(15,1)), sg.InputText(key = "Subat")],
-    [sg.Text("Mart Ayı:", size=(15,1)), sg.InputText(key = "Mart")],
-    [sg.Text("Nisan Ayı:", size=(15,1)), sg.InputText(key = "Nisan")],
-    [sg.Text("Mayıs Ayı:", size=(15,1)), sg.InputText(key = "Mayis")],
-    [sg.Text("Haziran Ayı:", size=(15,1)), sg.InputText(key = "Haziran")],
-    [sg.Text("Temmuz Ayı:", size=(15,1)), sg.InputText(key = "Temmuz")],
-    [sg.Text("Ağustos Ayı:", size=(15,1)), sg.InputText(key = "Agustos")],
-    [sg.Text("Eylül Ayı:", size=(15,1)), sg.InputText(key = "Eylul")],
-    [sg.Text("Ekim Ayı:", size=(15,1)), sg.InputText(key = "Ekim")],
-    [sg.Text("Kasım Ayı:", size=(15,1)), sg.InputText(key = "Kasim")],
-    [sg.Text("Aralık Ayı:", size=(15,1)), sg.InputText(key = "Aralik")],
+    [sg.Text("January:", size=(15,1)), sg.InputText(key = "Ocak")],
+    [sg.Text("February:", size=(15,1)), sg.InputText(key = "Subat")],
+    [sg.Text("March:", size=(15,1)), sg.InputText(key = "Mart")],
+    [sg.Text("April:", size=(15,1)), sg.InputText(key = "Nisan")],
+    [sg.Text("May:", size=(15,1)), sg.InputText(key = "Mayis")],
+    [sg.Text("June:", size=(15,1)), sg.InputText(key = "Haziran")],
+    [sg.Text("July:", size=(15,1)), sg.InputText(key = "Temmuz")],
+    [sg.Text("August:", size=(15,1)), sg.InputText(key = "Agustos")],
+    [sg.Text("September:", size=(15,1)), sg.InputText(key = "Eylul")],
+    [sg.Text("October:", size=(15,1)), sg.InputText(key = "Ekim")],
+    [sg.Text("November:", size=(15,1)), sg.InputText(key = "Kasim")],
+    [sg.Text("December:", size=(15,1)), sg.InputText(key = "Aralik")],
     
     [sg.Submit(), sg.Button("Clear"), sg.Exit()]
 ]
@@ -48,14 +49,24 @@ while True:
     if event == "Submit":
         
         tuketim_list = list(value.values())
-        print(tuketim_list)
         
         i = 0
         for x in tuketim_list:
-            a = ["E4","F4","G4","H4","I4","J4","K4","L4","M4","N4","O4","P4"]
-            ws[a[i]].value = int(x)
-            wb.save("CHP_verim.xlsx")
-            i = i + 1
+            
+            x = x.lstrip().rstrip()
+            
+            if re.match("^\d*$", x) == None or x == '' :
+                print("failed")
+                sg.popup("You must:\n-Fill all blanks\n-Use digits only")
+                quit()
+            else:
+                print("passed")
+                a = ["E4","F4","G4","H4","I4","J4","K4","L4","M4","N4","O4","P4"]
+                ws[a[i]].value = int(x)
+                print(x)
+                wb.save("CHP_verim.xlsx")
+                i = i + 1
+
         sg.popup("Data Saved!")
         break
 
